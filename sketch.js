65cae227f3e045cd92d07ebe1be1c20a634ed9ee
rachelebarazzetta <rachele.baraz@gmail.com>
2018-10-15 17:38:18 +0200

    details

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ var x = [],
   insectNum = 20,
   insectBody = 20;
 
-var theLightBull = {};
+var LightBullOn;
+var LightBullOff;
+var LightSign;
 
 var k = 0;
 
@@ -33,18 +35,19 @@ function mouseClicked() {
 function draw() {
 
   if (k == 0) {
-    background('blue');
+    background('#044389');
     dragInsect(0, mouseX, mouseY);
     for (var i = 0; i < x.length - 1; i++) {
       dragInsect(i + 1, x[i], y[i]);
-      lightBull(mouseX, mouseY, 50);
+    LightBullOn = new  lightBull(mouseX, mouseY, 50, color(242, 155, 18), color(240, 195,15), color(229, 126, 34));
+    LightSign = new  light(mouseX, mouseY, 7);
     }
   } else if (k == 1) {
-    background('green');
+    background('#29335C');
     randomInsect(0, random(), random());
     for (var i = 0; i < x.length - 1; i++) {
       randomInsect(i + 1, x[i], y[i]);
-      lightBullOff(mouseX, mouseY, 50);
+      LightBullOff = new  lightBull(mouseX, mouseY, 50, color(24, 170, 234), color(20, 206, 237), color(36, 139, 244));
     }
   }
 
@@ -58,8 +61,8 @@ function dragInsect(i, xin, yin) {
   var dx = xin - x[i];
   var dy = yin - y[i];
   var angle = atan2(dy, dx);
-  x[i] = xin - cos(angle) * insectBody * random(0, 5);
-  y[i] = yin - sin(angle) * insectBody * random(0, 5);
+  x[i] = xin - cos(angle) * insectBody * random(0, 4);
+  y[i] = yin - sin(angle) * insectBody * random(0, 4);
   Insect(x[i], y[i], angle);
 }
 
@@ -74,62 +77,88 @@ function randomInsect(i, xin, yin) {
 
 
 function Insect(x, y, a) {
+  push();
+  translate(x - 30, y - 30);
+  rotate(70);
+  fill('#14ceff');
+  ellipse(-2, 0, 17, 6);
+  rotate(-70);
+  ellipse(-2, 0, 17, 6);
+  pop();
+
   angleMode(DEGREES);
   push();
   translate(x - 30, y - 30);
   noStroke();
 
-  rotate(70 * a);
-  fill('lightblue');
-  ellipse(-2, 0, 13, 5);
-  rotate(-70 * a);
-  ellipse(-2, 0, 13, 5);
-
   rotate(a);
   fill(0);
-  ellipse(0, 0, 13, 6)
+  ellipse(0, 0, 14, 10);
+
+  fill('white');
+  noStroke();
+  ellipse(+5, -2, 2, 2);
+  ellipse(+5, +2, 2, 2);
+
+  stroke('yellow');
+  strokeWeight(2);
+  noFill();
+  line(-1, 3.5, -1, -3.5)
   pop();
+
+
+
 }
 
-function lightBull(x, y, diameter) {
+function lightBull(x, y, diameter, color1 , color2, color3) {
   angleMode(DEGREES);
-  this.color = 'orange';
-  fill(this.color);
   noStroke();
-  ellipse(x, y, diameter);
-  rect(x - 13, y + 4, 26, 26, 6)
+  fill(color2);
+  arc(x, y, diameter, diameter, 90, 270, PIE);
+  quad(x,y,x-25,y,x-13,y+30, x,y+30);
 
-  fill(100);
-  rect(x - 13, y + 33, 26, 7, 40)
-
-  fill(110);
-  rect(x - 9.5, y + 43, 19, 7, 40)
+  fill(color1);
+  arc(x, y, diameter, diameter, 270, 90, PIE);
+  quad(x,y,x+25,y,x+13,y+30, x,y+30);
 
   noFill();
-  stroke('white');
-  strokeWeight(10);
-  arc(x, y, 30, 30, 270, 0);
-}
+  strokeWeight(2);
+  stroke(color3);
+  line(x+4,y+30,x+14,y);
+  line(x-4,y+30,x-14,y);
+  line(x-14,y,x-6,y+6);
+  line(x,y,x-6,y+6);
+  line(x+14,y,x+6,y+6);
+  line(x,y,x+6,y+6);
 
-function lightBullOff(x, y, diameter, color) {
-  angleMode(DEGREES);
-  this.color = 'black';
-  fill(this.color);
   noStroke();
-  ellipse(x, y, diameter);
-  rect(x - 13, y + 4, 26, 26, 6)
+  fill(127, 140, 141);
+  arc(x,y+40,20,20,0,180,PIE);
+  rect(x - 13, y + 30, 13, 3);
+  rect(x - 13, y + 36, 13, 3);
 
-  fill(100);
-  rect(x - 13, y + 33, 26, 7, 40)
+  fill(149, 164, 165);
+  rect(x - 13, y + 33, 13, 3);
+  rect(x - 13, y + 39, 13, 3);
+  rect(x, y + 30, 13, 3);
+  rect(x, y + 36, 13, 3);
 
-  fill(110);
-  rect(x - 9.5, y + 43, 19, 7, 40)
-
-  noFill();
-  stroke('white');
-  strokeWeight(10);
-  arc(x, y, 30, 30, 270, 0);
+  fill(189, 194, 197);
+  rect(x, y + 33, 13, 3);
+  rect(x, y + 39, 13, 3);
 }
+
+function light(x, y){
+  noFill();
+  strokeWeight(4);
+  stroke(240, 195,15, 90);
+  arc(x, y, 80, 80, 130, 50);
+  stroke(240, 195,15, 20);
+  arc(x, y, 115, 115, 130, 50);
+  stroke(240, 195,15, 10);
+  arc(x, y, 150, 150, 130, 50);
+}
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
